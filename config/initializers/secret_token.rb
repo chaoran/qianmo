f9a0d6b0c1382bb2e378f75afbc4901072cfb0a3
Chaoran Yang <chaorany@me.com>
2012-06-11 23:41:06 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qianmo::Application.config.secret_token = '61d21633307118a3caa8e4ca1779eb1a1880384c4f81a933a8101c6e34e6db6cc2f60f10fc501216965d308fdc807734efaa1fee4f47d2f7b9aa04d4691e30ce'

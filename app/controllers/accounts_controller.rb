26183525a9f5285936e06978a46e083962740a5b
Chaoran Yang <chaorany@me.com>
2012-06-12 23:02:51 -0500

    Fix a bug causing the message "You've already signed in." to show when user access homepage with logout

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -18,6 +18,24 @@ class AccountsController < Devise::RegistrationsController
   def after_sign_up_path_for(resource)
     edit_user_profile_path(resource)
   end
+  
+  # Copied from devise. comment out set alert message.
+  def require_no_authentication
+    assert_is_devise_resource!
+    return unless is_navigational_format?
+    no_input = devise_mapping.no_input_strategies
 
+    authenticated = if no_input.present?
+      args = no_input.dup.push :scope => resource_name
+      warden.authenticate?(*args)
+    else
+      warden.authenticated?(resource_name)
+    end
+
+    if authenticated && resource = warden.user(resource_name)
+      #flash[:alert] = I18n.t("devise.failure.already_authenticated")
+      redirect_to after_sign_in_path_for(resource)
+    end
+  end
 end
 

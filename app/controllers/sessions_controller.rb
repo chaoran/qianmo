f5ed5265f0d867ff45eaceda513b366cf1fc79da
Chaoran Yang <chaorany@me.com>
2012-06-12 21:52:33 -0500

    Changed the login form on top bar to use form_tag, to avoid show error messages returned from invalid sign_at form.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < Devise::SessionsController
+  before_filter :setup_user_params, :only => :create
   def create
     resource = warden.authenticate!(auth_options)
     #set_flash_message(:notice, :signed_in) if is_navigational_format?
@@ -19,6 +20,16 @@ class SessionsController < Devise::SessionsController
       format.all do
         head :no_content
       end
+    end
+  end
+  
+  private
+  def setup_user_params
+    unless params[:user]
+      params[:user] = {}
+      params[:user][:email] = params[:email]
+      params[:user][:password] = params[:password]
+      params[:user][:remember_me] = params[:remember_me]
     end
   end
 end

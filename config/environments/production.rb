4146a3c5ca41a6b0c059d36fe1acf1f2dbc46d44
Chaoran Yang <chaorany@me.com>
2012-06-12 19:53:17 -0500

    Config devise to use sendgrid to send email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,4 +65,16 @@ Qianmo::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   
+  config.action_mailer.default_url_options = { :host => 'chaorany.org' }
+  ActionMailer::Base.smtp_settings = {
+    :address        => "smtp.sendgrid.net",
+    :port           => "25",
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['SENDGRID_DOMAIN']
+  }
+  
+  config.weibo_key = '1857236140'
+  config.weibo_secret = '3e91c6608b81a80aceb790c61a723269'
 end
